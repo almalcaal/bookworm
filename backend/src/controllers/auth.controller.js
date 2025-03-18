5beb1346dd03a5c8fe1c4dd89010957b28a313be
Aldrin Caalim <aldrincaalim@aldrins-air.mynetworksettings.com>
2025-03-18 15:54:11 -0400

    register completed

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,99 @@
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+
+// @desc            Register a new user
+// @route           POST /api/auth/register
+// @access          Public
+export const registerUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Full name, email and password are required inputs" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+
+    if (username.length < 3) {
+      return res
+        .status(400)
+        .json({ message: "Username must be at least 3 characters" });
+    }
+
+    const existingUsername = await User.findOne({ username });
+    if (existingUsername) {
+      return res.status(400).json({ message: "Username already in use" });
+    }
+
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    const randomImage = `https://api.dicebear.com/9.x/thumbs/svg?seed=${username}`;
+
+    const user = await User.create({
+      username,
+      email,
+      password,
+      profileImage: randomImage,
+    });
+
+    if (user) {
+      await user.save();
+
+      //   token will be used to authenticate user giving them permission to CRUD book posts. so client will be using the token and the server will check who owns the token
+      const token = generateToken(user._id);
+      res.status(201).json({
+        token,
+        user: {
+          _id: user._id,
+          username: user.username,
+          email: user.email,
+          profilePic: user.profilePic,
+        },
+      });
+    }
+  } catch (err) {
+    console.log(`ERROR in registerUser controller`, err.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+// @desc            Authenticate user / get token
+// @route           POST /api/auth/login
+// @access          Public
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const correctPassword = await user.matchPassword(password);
+    if (!correctPassword) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    generateToken(res, user._id);
+
+    res.status(200).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (err) {
+    console.log("Error in loginUser controller", err.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,10 @@
 import express from "express";
 
+import { registerUser } from "../controllers/auth.controller.js";
+
 const router = express.Router();
 
-router.post("/register", async (req, res) => {
-  res.send("register");
-});
+router.post("/register", registerUser);
 
 router.post("/login", async (req, res) => {
   res.send("login");
