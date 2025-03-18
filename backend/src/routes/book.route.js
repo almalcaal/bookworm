de4388c0a3caee6d69f61a48f459d02d24f71e8d
Aldrin Caalim <aldrincaalim@aldrins-air.mynetworksettings.com>
2025-03-18 16:58:09 -0400

    feature added

--- backend/src/routes/book.route.js
+++ backend/src/routes/book.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createPost } from "../controllers/book.controller.js";
+import { createPost, getPosts } from "../controllers/book.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/", protectRoute, createPost);
+router.get("/", protectRoute, getPosts);
 
 export default router;
